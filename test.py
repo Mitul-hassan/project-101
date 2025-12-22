668f2b66dbf64722a1e362004c0c81d111eff180
Mitul-hassan <mitulmahamudulhassan@gmail.com>
2025-12-23 01:47:22 +0600

    Day-13 halh completed,yet not fully completed

--- test.py
+++ test.py
@@ -707,7 +707,7 @@ myset={'apple','banana','cherrry'}
 for x in myset:
     print(x)
 
-                      # Day-12 Learning 📅
+                     # Day-12 Learning 📅
                     # Starting Time: 12:30 am 🕐
                     # Ending Time: 1:30 am 🕐
 '''
@@ -886,3 +886,63 @@ b = frozenset({2, 3})
 print(a.union(b))
 print(a | b)
 
+                    # Day-13 Learning 📅
+                    # Starting Time: 1:20 am 🕐
+                    # Ending Time: : am 🕐
+'''
+1.Puthon Dictionaries
+2.Access in dictionarie
+'''    
+#Dictionaries are used to store data values in key:value pairs.A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+thisdict={
+    "brand": "ford",
+    "model":"mustang",
+    'year' :1964
+}  
+print(thisdict)
+
+#Dictionary items are ordered, changeable, and do not allow duplicates.Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+thisdict={
+    'brand':'ford',
+    'model':'mustag',
+    'year': 1964
+}
+print(thisdict['brand'])    
+
+#When we say that dictionaries are ordered, it means that the items have a defined order, and that order will not change.
+#Unordered means that the items do not have a defined order, you cannot refer to an item by using an index.
+
+#Changeable:Dictionaries are changeable, meaning that we can change, add or remove items after the dictionary has been created.
+#Duplicates Not Allowed:Dictionaries cannot have two items with the same key.
+#Duplicate values will overwrite existing values:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+#Dictionary Length:To determine how many items a dictionary has, use the len() function:
+#Print the number of items in the dictionary:
+print(len(thisdict))
+
+#Dictionary Items - Data Types.The values in dictionary items can be of any data type:
+#String, int, boolean, and list data types:
+thisdict={
+    'brand':'ford',
+    'electric':False,
+    'year': 1964,
+    'colors':['red','wehite','blue']
+}
+#From Python's perspective, dictionaries are defined as objects with the data type 'dict':
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+#The dict() Constructor:It is also possible to use the dict() constructor to make a dictionary.
+thisdict=dict(name='john',age=36,country='norway')
+print(thisdict)
