ca112c8774042477f1cc7f9b8df23e5312f1ca17
Mitul-hassan <mitulmahamudulhassan@gmail.com>
2025-12-24 02:07:20 +0600

    remaning task of day-13 has completed

--- test.py
+++ test.py
@@ -946,3 +946,88 @@ print(type(thisdict))
 #The dict() Constructor:It is also possible to use the dict() constructor to make a dictionary.
 thisdict=dict(name='john',age=36,country='norway')
 print(thisdict)
+
+#2.Access Dictionary
+#Accessing items:we can access the items of a dictionary by referring to its key name, inside square brackets
+thisdict={
+    'brand':'Foard',
+    'model':'Mustang',
+    'year': 1964
+}
+x=thisdict['model']
+print(x)
+#There is also a method called get() that will give the same result:
+x=thisdict.get('year')
+print(x)
+
+#Get keys:The keys() method will return a list of all the keys in the dictionary.
+x=thisdict.keys()
+print(x)
+#The list of the keys is a view of the dictionary, meaning that any changes done to the dictionary will be reflected in the keys list.
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.keys()
+print(x) #before the change
+car["color"] = "white"
+print(x) #after the change
+
+#Get Values:The values() method will return a list of all the values in the dictionary.
+x=car.values()
+print(x)
+#Make a change in the original dictionary, and see that the values list gets updated as well:
+car={
+    'brand':'foard',
+    'model':'mustang',
+    'year':'1964'
+}
+x=car.values()
+print(x) #before the change
+car['year']=2010
+print(x) #after the change
+#Add a new item to the original dictionary, and see that the values list gets updated as well
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.values()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change
+
+#Get Items:The items() method will return each item in a dictionary, as tuples in a list.
+x=car.items()
+print(x)
+#The returned list is a view of the items of the dictionary, meaning that any changes done to the dictionary will be reflected in the items list.
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.items()
+print(x) #before the change
+car["year"] = 2020
+print(x) #after the change
+#Add a new item to the original dictionary, and see that the items list gets updated as well:
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.items()
+print(x) #before the change
+car["color"] = "red"
+print(x) #after the change
+
+#Check if Key Exists:o determine if a specified key is present in a dictionary use the in keyword:
+thisdict={
+    'brand':'Foard',
+    'model':'Mustang',
+    'year':1964
+}
+if 'model' in thisdict:
+    print('yes')
